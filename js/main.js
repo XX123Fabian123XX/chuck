089a7b67501640f77b04daa17dc4f91fe7c87e16
fabian <fabian@fabian-MS-7A38.speedport.ip>
2020-08-10 12:45:14 +0200

    final commit

--- js/main.js
+++ js/main.js
@@ -1,24 +1,4 @@
-document.getElementById("button").addEventListener("click",async () => {
-    displayFact( await getFact());
-} )
-
-
-const getFact = async () => {
+document.getElementById("button").addEventListener("click", async () => {
     const data = await (await (await fetch("https://api.chucknorris.io/jokes/random")).json());
-    console.log(data.value.length);
-    return data.value;
-}
-
-const displayFact = fact => {
-    const text = document.getElementById("text")
-    if (fact.length > 400) {
-        text.style.fontSize = "2.1"
-    }
-    else if (fact.length > 180) {
-        text.style.fontSize = "2.5rem"
-    } else {
-        text.style.fontSize="3rem";
-    }
-
-    text.textContent = fact
-}
+    document.getElementById("text").textContent = data.value;
+})
